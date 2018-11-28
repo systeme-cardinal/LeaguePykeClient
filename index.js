93bf63b59ee4215fced7ea0452145b58aebff445
Jason Bierque <bierquejason@gmail.com>
2018-11-28 12:24:49 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+/* For start this API, login on your client League of Legends */ 
+/* This API is not supported for RiotGames */ 
+
+const lcu = require('lcu-connector');
+const connect = new lcu();
+const got = require('got');
+
